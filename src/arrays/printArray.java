7d45655382c089b61597f6a3e269d19d52c73b19
Vidhi Khakhar <v.k.khakhar@student.tudelft.nl>
2025-08-14 12:57:24 +0200

    Added comment and "how to reverse an array"

--- src/arrays/printArray.java
+++ src/arrays/printArray.java
@@ -7,6 +7,7 @@ public class printArray {
     public static void main(String[] args) {
         int[] example1 = {2, 4, 6, 8, 10};
         println(example1);
+        reverse(example1);
 
     }
 
@@ -23,6 +24,26 @@ public class printArray {
             }
         }
         System.out.println();
+    }
+
+    /**
+     * Reverse an array
+     * @param seq - random integer array
+     */
+    public static void reverse(int[] seq){
+        if (seq == null || seq.length < 2) {
+            throw new IllegalArgumentException("The array must contain two or more value");
+        } else {
+            for (int i = 0; i <= seq.length/2; i++) {
+                int temp = seq[i];
+                seq[i] = seq[(seq.length - 1) - i];
+                seq[(seq.length - 1) - i] = temp;
+            }
+
+            for (int n : seq) {
+                System.out.print(n + " ");
+            }
+        }
     }
 
 }
